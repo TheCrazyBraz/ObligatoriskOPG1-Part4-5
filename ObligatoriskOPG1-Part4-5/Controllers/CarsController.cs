2b3f7a030d49924caf4b47bef6f84d38c7cf9e5f
marcl <marcl@MARC-SKOLEPC>
2023-03-06 23:33:43 +0100

    Add project files.

---
+++ ObligatoriskOPG1-Part4-5/Controllers/CarsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObligatoriskOPG1;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ObligatoriskOPG1_Part4_5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private Repository repository = new Repository();
+        // GET: api/<CarsController>
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return repository.getAllCars();
+        }
+
+
+        // GET api/<CarsController>/5
+        [HttpGet("{id}")]
+        public Car Get(int id)
+        {
+            return repository.GetCarById(id);
+        }
+
+        // POST api/<CarsController>
+        [HttpPost]
+        public Car Post([FromBody] Car car)
+        {
+            return repository.AddCar(car);
+        }
+
+        // PUT api/<CarsController>/5
+        [HttpPut("{id}")]
+        public Car Put(int id, [FromBody] Car car)
+        {
+            return repository.UpdateCar(id, car);
+        }
+
+        // DELETE api/<CarsController>/5
+        [HttpDelete("{id}")]
+        public Car Delete(int id)
+        {
+            return repository.DeleteCar(id);
+        }
+    }
+}
---
+++ ObligatoriskOPG1-Part4-5/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using ObligatoriskOPG1;
+using System.Collections.Generic;
+using System;
+
+namespace ObligatoriskOPG1_Part4_5
+{
+    public class Repository
+    {
+        static int nextId = 0;
+        static List<Car> cars = new List<Car>()
+        {
+            new Car() {Id = nextId++, Model = "Ford", Plate = "ABC123", Price = 89000},
+            new Car() {Id = nextId++, Model = "Kia", Plate = "XYZ123", Price = 890030},
+            new Car() {Id = nextId++, Model = "Audi", Plate = "ABC987", Price = 890200},
+            new Car() {Id = nextId++, Model = "Mercedes", Plate = "XYZ987", Price = 895000}
+        };  
+        public List<Car> getAllCars()
+        {
+            return cars;
+        }
+        [HttpGet("{id:int}")]
+        public Car GetCarById(int id)
+        {
+            var car = cars.FirstOrDefault(c => c.Id == id);
+            return car;
+        }
+
+        public Car AddCar(Car car)
+        {
+            car.Id= nextId++;
+            cars.Add(car);
+            return car;
+        }
+
+        public Car DeleteCar(int id)
+        {
+            var car = cars.Find(c => c.Id == id);
+            cars.Remove(car);
+
+
+            return car;
+        }
+        public Car UpdateCar(int id, Car car)
+        {
+
+            Car findCar = GetCarById(id);
+
+            findCar.Model = car.Model;
+            findCar.Plate = car.Plate;
+            findCar.Price = car.Price;
+
+            return findCar;
+
+        } 
+       
+    }
+}
